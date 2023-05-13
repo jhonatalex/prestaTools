c11d00e16f9306e834a4c5022a100491ffb0c25d
JhonatalexZenta <jhonatan.mejias@zentagroup.com>
2023-05-12 22:31:48 -0400

    nuevo

---
+++ prestaToolsApi/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prestaToolsApi.Controllers
+{
+
+
+    [ApiController]
+    [Route("User")]
+    public class UserController : Controller
+    {
+
+
+
+        [HttpGet]
+        [Route("listar")]
+        public dynamic getUsers() {
+
+            //TODO TODA LA LOGICA para buscar el usuarios en la base de datos
+
+            List<User> users = new List<User>
+            {
+
+                new User
+                {
+                    id = 1,
+                    name = "Hialmar",
+                    email = "uno@uno.com",
+                    password = "1234",
+                },
+                new User
+                {
+                    id = 2,
+                    name = "RicARDO",
+                    email = "DOS@uno.com",
+                    password = "1234",
+                }
+            };
+
+        return users;
+
+        }
+
+
+
+
+
+    };
+
+
+
+
+};
---
+++ prestaToolsApi/model/User.cs
@@ -0,0 +1,13 @@
+﻿namespace prestaToolsApi
+{
+    public class User
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+
+    }
+}
