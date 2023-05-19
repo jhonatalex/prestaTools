b2d13e93c8e8694cdc5c3185897c51c4a821b0af
JhonatalexZenta <jhonatan.mejias@zentagroup.com>
2023-05-18 22:30:49 -0400

    add category and Lender model

--- prestaToolsApi/model/Lender.cs
+++ prestaToolsApi/model/Lender.cs
@@ -1,30 +1,33 @@
-﻿namespace prestaToolsApi
+﻿namespace prestaToolsApi.model
 {
-    public class User
+    public class Lender
     {
 
         public int id { get; set; }
         public string name { get; set; }
-        
+        public string d_identidad { get; set; }
+
         public string last_name { get; set; }
         public string email { get; set; }
         public string password { get; set; }
 
         public string phone { get; set; }
 
-        public string adress { get; set; }
+        public string addres { get; set; }
         public string city { get; set; }
         public string country { get; set; }
 
-        public string zipcode { get; set; }
+        public string number_bank { get; set; }
 
-        public string date { get; set; }
+        public string balance_wallet { get; set; }
 
         public DateTime created_at { get; set; }
-   
-        public bool state { get; set;}
 
-        public string d_identidad { get; set; }
+        public bool state { get; set; }
+
+     
+
+
 
     }
 }
