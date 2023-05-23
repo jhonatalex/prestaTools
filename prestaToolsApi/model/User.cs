9f5aa3dfcd6fb33da30daa99e41b840d747d6d73
Ricardo J. Rojas A <ricardo.rojas@esmtconsulting.com>
2023-05-23 16:46:59 -0500

    Se creó conexión y se realizó el método insertar

--- prestaToolsApi/model/User.cs
+++ prestaToolsApi/model/User.cs
@@ -5,26 +5,14 @@
 
         public int id { get; set; }
         public string name { get; set; }
-   
         public string last_name { get; set; }
-        public string email { get; set; }
         public string password { get; set; }
-
-        public string phone { get; set; }
-
-        public string adress { get; set; }
-        public string city { get; set; }
-        public string country { get; set; }
-
-        public string zipcode { get; set; }
-
-        public string date { get; set; }
-
-        public DateTime created_at { get; set; }
-   
-        public bool state { get; set;}
-
+        public string email { get; set; }
+        public string telephone { get; set; }
+        public string address { get; set; }
         public string d_identidad { get; set; }
+        public DateTime date { get; set; }
+        public bool state { get; set;}
 
     }
 }
