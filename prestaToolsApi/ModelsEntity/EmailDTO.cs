09d11fd783ad7a7028b744e7ad6533244ab54372
Ricardo J. Rojas A <ricardo.rojas@esmtconsulting.com>
2023-09-23 18:41:21 -0500

    comimt

---
+++ prestaToolsApi/ModelsEntity/EmailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace prestaToolsApi.ModelsEntity
+{
+    public class EmailDTO
+    {
+        public string Para { get; set; }=string.Empty;
+        public string Contenido { get; set; } = string.Empty;
+        public string Asunto { get; set; } = string.Empty;
+    }
+}
---
+++ prestaToolsApi/ModelsEntity/EmailService.cs
@@ -0,0 +1,68 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
+using MimeKit;
+using System.Drawing.Text;
+using System.Net;
+using System.Runtime.Intrinsics.X86;
+
+namespace prestaToolsApi.ModelsEntity
+{
+    public class EmailService : IEmailService
+    {
+
+        private readonly IConfiguration _config;
+        public EmailService(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public Task SendAsyncronousEmail(string email, string subject, string message)
+        {
+
+            /*
+             * NO FUNCIONO 
+             * 
+             * 
+            var origin_email = "ventas@prestatools.cl";
+            var password = "Prestatools.2023";
+            var client = new SmtpClient("smtp.hostinger.com", 465)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(origin_email, password)
+            };
+            return client.SendMailAsync(new MailMessage(from:origin_email,to:email, subject, message));
+            
+            */
+            throw new NotImplementedException();
+        }
+
+        void IEmailService.SendEmail(EmailDTO request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("Email:UserName").Value));
+            email.To.Add(MailboxAddress.Parse(request.Para));
+            email.Subject = request.Asunto;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = request.Contenido    
+            };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(
+                _config.GetSection("Email:Host").Value,
+                 Convert.ToInt32(_config.GetSection("Email:Port").Value),
+                 SecureSocketOptions.StartTls
+                 
+               );
+            smtp.Authenticate(
+                _config.GetSection("Email:UserName").Value,
+                _config.GetSection("Email:Password").Value
+                );
+
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+        }
+    }
+}
